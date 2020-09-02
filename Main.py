6f6a6860c7027b8dc7e505eec4dec8727fad3bd2
SHAILESH MATANG <54760396+SMATANG@users.noreply.github.com>
2020-09-02 22:44:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ def menu():
       while True:
            try:
                 print("═══════════════════════╗")
-                x = str(input('\033[1;92m • [ SJH2130] >  \033[1;93m:                  >
+                x = str(input('\033[1;92m • [ SJH2130] >  \033[1;93m:'))
                 e = getpass('\033[1;92m • [Password] >  \033[1;93m: ')
                 print("═══════════════════════╝")
                 if x=="S" and e=="0":
